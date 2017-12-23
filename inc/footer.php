859ec535427030f1d1381297bf76be374202e9c8
mdrajibraj <34791789+mdrajibraj@users.noreply.github.com>
2017-12-23 07:55:16 -0800

    Add files via upload

---
+++ inc/footer.php
@@ -0,0 +1,10 @@
+<div class="well">
+         
+       	 <h3>
+       	 	SpiderBd
+       	 <span class="pull-right">Spiderbd.com</span>
+       	 </h3>
+       </div>
+	</div>
+	</body>
+</html>
---
+++ inc/header.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Employee Attendance Management System</title>
+	<link rel="stylesheet" href="inc/bootstrap.min.css" media="screen" title="no title"/>
+	<script type="text/javascript" src="inc/jquery.min.js"></script>
+    <script type="text/javascript" src="inc/bootstrap.min.js"></script>
+     
+</head>
+<body>
+	<div class="container">
+	   <div class="well text-center">
+	   	  <h2>Employee Attendance Management System</h2>
+	</div>
